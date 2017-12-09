b153186e2ee9c50329e488a66e0f529d3525b387
solomonchild <solomonchild@gmail.com>
2017-12-09 13:03:07 +0200

    Finished task9

--- cpp/src/build.bat
+++ cpp/src/build.bat
@@ -2,7 +2,7 @@
 
 set NUM=%1
 if exist task%NUM%.txt (
-    clang++ -I..\inc\ -Wall -Wextra -Werror -pedantic task%NUM%.cpp -o task%NUM%.exe --std=c++14 && task%NUM%.exe < task%NUM%.txt
+    clang++ -Xclang -flto-visibility-public-std -I..\inc\ -Wall -Wextra -Werror -pedantic task%NUM%.cpp -o task%NUM%.exe --std=c++14 && task%NUM%.exe < task%NUM%.txt
 ) else (
     clang++ -I..\inc\ -Wall -Wextra -Werror -pedantic task%NUM%.cpp -o task%NUM%.exe --std=c++14 && task%NUM%.exe 
 )
---
+++ cpp/src/task9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <numeric>
+
+int main() {
+    enum class State{ 
+        Reading,
+        Skip,
+        Comment,
+    };
+
+    using Tuple = std::pair<size_t, size_t>;
+    Tuple t = std::accumulate(std::istream_iterator<char>(std::cin), {}, Tuple(), [level = 0, state = State::Reading] (const Tuple& tuple, char c) mutable {
+        Tuple ret = tuple;
+        switch(state) {
+            case State::Skip: {
+              state = State::Comment;
+            } break;
+            case State::Reading: {
+                if(c == '{') {
+                    level++;
+                }		
+                if(c == '}') {
+                    ret = std::make_pair(tuple.first + level, tuple.second);
+                    level--;
+                }
+                if(c == '<') {
+                    state = State::Comment;
+                }
+            } break;
+            case State::Comment: {
+                if(c == '>') state = State::Reading;
+                else if(c == '!') state = State::Skip;
+                else ret = std::make_pair(tuple.first, tuple.second + 1);
+            } break;
+        }
+        return ret;
+    });
+    std::cout << t.first << std::endl;
+    //11347
+    std::cout << t.second << std::endl;
+    //5404
+}
