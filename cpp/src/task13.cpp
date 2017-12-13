ed6f86d4ab0442faeecedf6e43eaab1f1edc069d
Vladislavs Burakovs <solomonchild@gmail.com>
2017-12-13 13:54:15 +0200

    Refactored day13

--- cpp/src/task13.cpp
+++ cpp/src/task13.cpp
@@ -1,44 +1,31 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <numeric>
 #include <utility>
 
 int main(int, char**) {
-    using Ints = std::vector<int>;
-    Ints layers;
-    std::string line;
-    for(std::string line; std::getline(std::cin, line);) {
-        std::istringstream ss(line);
+    using Walls = std::vector<std::pair<int, int>>;
+    Walls walls;
+    while(std::cin.good()) {
         std::string _;
         size_t layer;
         size_t depth;
-        ss >> layer >> _ >> depth;
-        if(layer >= layers.size()) {
-            size_t prev_size = layers.size();
-            layers.resize(layer + 1);
-            std::generate(layers.begin() + prev_size, layers.end(), [](){return 0;});
-        }
-        layers[layer] = depth;
+        std::cin >> layer >> _ >> depth;
+        walls.emplace_back(layer, depth);
     }
 
-    auto severity = [&layers] (int offset = 0, bool finish = true) {
+    auto severity = [&walls] (int offset = 0, bool finish = true) {
         bool caught = false;
         int sev = 0;
-        for(size_t i = 0u; i < layers.size(); i++) {
-            if(!(i + offset) || (i + offset)%((layers[i] * 2 - 2)) == 0) {
-                sev += i * layers[i];
+        for(auto w : walls) {
+            if(!(w.first + offset) || (w.first + offset)%((w.second * 2 - 2)) == 0) {
+                sev += w.first * w.second;
                 caught = true;
                 if(!finish)
                     break;
             }
         }
-
-        if(sev == 0 && caught) {
-            return -1;
-        }
+        if(sev == 0 && caught) return -1;
         else return sev;
     };
     auto sev = severity();
