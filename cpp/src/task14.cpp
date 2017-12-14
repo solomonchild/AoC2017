8e276f53372103122e0ea97dbcff316cdde06afb
Vladislavs Burakovs <solomonchild@gmail.com>
2017-12-14 11:28:07 +0200

    Checkpoint

--- cpp/src/task14.cpp
+++ cpp/src/task14.cpp
@@ -49,15 +49,13 @@ size_t part1(const std::string& row_ascii) {
             return count + c;
     });
 }
-auto part2(const std::string& hash) {
+using Rows = std::array<std::array<int, 128>, 128>;
+auto part2(const std::string& hash, Rows& rows, size_t rownum) {
 
-    using Rows = std::array<std::array<int, 128>, 128>;
-    Rows rows;
     std::array<int, 128> row{};
     for(size_t i = 0; i < hash.size(); i++) {
         std::string tmp(1, hash[i]);
         auto chr = std::stoi(tmp, 0, 16);
-        std::cout << chr << std::endl;
 
         for(size_t idx = 0; idx < 4; idx++) {
             bool set = ((chr >> (3-idx)) & 0x1);
@@ -66,33 +64,43 @@ auto part2(const std::string& hash) {
             }
         }
     }
-    //rows[rownum] = row;
-    auto get_left = [&rows](int row, int col){
-        if(col != 0) 
-            return rows[row][col-1];
-    };
-    auto get_right = [&rows](int row, int col){
-        if(col != 127) 
-            return rows[row][col+1];
-    };
-    auto get_up = [&rows](int row, int col){
-        if(row != 0) 
-            return rows[row-1][col];
-    };
-    auto get_down = [&rows](int row, int col){
-        if(row != 127) 
-            return rows[row+1][col];
-    };
-
+    rows[rownum] = row;
+    const static auto invalid = std::numeric_limits<int>::min();
+    if(rownum == 127) {
+        auto get_left = [&rows](int row, int col){ if(col != 0) return rows[row][col-1]; else return invalid; };
+        auto set_left = [&rows](int row, int col, int val){ rows[row][col-1] = val;};
+        auto get_right = [&rows](int row, int col){ if(col != 127) return rows[row][col+1]; else return invalid; };
+        auto set_right = [&rows](int row, int col, int val){ rows[row][col+1] = val; };
+        auto get_up = [&rows](int row, int col){ if(row != 0) return rows[row-1][col]; else return invalid; };
+        auto set_up = [&rows](int row, int col, int val){ rows[row-1][col] = val; };
+        auto get_down = [&rows](int row, int col){ if(row != 127) return rows[row+1][col]; else return invalid; };
+        auto set_down = [&rows](int row, int col, int val){ rows[row+1][col] = val; };
+    
+    auto max_group = 1;
+    for(size_t row = 0; row < 127; row++) {
+        for(size_t col = 0; col < 127; col++) {
+            if(rows[row][col] == invalid)
+                continue;
+            if(get_left(row, col) == -1) set_left(row, col, max_group); else rows[row][col] = get_left(row,col);
+            if(get_right(row, col) == -1) set_right(row, col, max_group); else rows[row][col] = get_right(row,col);
+            if(get_down(row, col) == -1) set_down(row, col, max_group); else rows[row][col] = get_down(row,col);
+            if(get_up(row, col) == -1) set_up(row, col, max_group); else rows[row][col] = get_up(row,col);
+            if(rows[row][col] == -1) rows[row][col] = max_group++;
+        }
+    }
+    std::cout << max_group << std::endl;
+    }
 }
 
 int main(int, char**){
     std::string hash; 
     std::getline(std::cin, hash);
     size_t count = 0;
+    Rows rows;
     for(size_t rownum = 0; rownum < 128; rownum++) {
         auto row_ascii = get_hash(hash + "-" + std::to_string(rownum));
         count += part1(row_ascii);
+        part2(row_ascii, rows, rownum);
     }
     std::cout << count << std::endl;
 }
