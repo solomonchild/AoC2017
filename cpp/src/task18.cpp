b71a5117c0efc54fab175c2b9ad53e1f9eeb3701
Vladislavs Burakovs <solomonchild@gmail.com>
2017-12-19 17:13:17 +0200

    Refactored task18

--- cpp/src/task18.cpp
+++ cpp/src/task18.cpp
@@ -2,263 +2,133 @@
 #include <iostream>
 #include <map>
 #include <cctype>
-#include <utility>
 #include <cstdint>
-#include <condition_variable>
-#include <thread>
-#include <mutex>
-#include <chrono>
 #include <deque>
-#include <sstream>
-#include <cassert>
+#include <functional>
 
 #include "common.hpp"
 
 enum class CommandType {
-    Sound,
+    Send,
     Set,
     Add,
     Mul,
     Mod,
-    Recover,
+    Receive,
     JumpGZ,
 };
+using Command = std::pair<CommandType, std::vector<std::string>>; 
+using Commands = std::vector<Command>;
+using Registers = std::map<char, int64_t>;
 
 CommandType string_to_cmd_type(const std::string& str) {
-    if(str == "snd") return CommandType::Sound;
+    if(str == "snd") return CommandType::Send;
     if(str == "set") return CommandType::Set;
     if(str == "add") return CommandType::Add;
     if(str == "mul") return CommandType::Mul;
     if(str == "mod") return CommandType::Mod;
-    if(str == "rcv") return CommandType::Recover;
+    if(str == "rcv") return CommandType::Receive;
     else return CommandType::JumpGZ;
 }
 
+auto get_val (const std::string& arg, Registers& registers) {
+    if(std::isalpha(arg[0])) {
+        return registers[arg[0]];
+    } else return static_cast<int64_t>(std::stoll(arg));
+};
 
-using Command = std::pair<CommandType, std::vector<std::string>>; 
-using Commands = std::vector<Command>;
-void part1(const Commands& cmds) {
-    std::map<char, int64_t> registers;
-    auto freq = 0U;
-    auto get_val = [&registers] (const std::string& arg) {
-        if(std::isalpha(arg[0])) {
-            return registers[arg[0]];
-        } else return static_cast<int64_t>(std::stoll(arg));
-    };
+void p(Registers& reg) {
+    for(const auto& p : reg) {
+        std::cout << "[" << p.first << "]: " << p.second << ", ";
+    }
+    std::cout << std::endl;
+}
 
-    for(size_t i = 0; i < cmds.size(); i++) {
-        const auto& p = cmds[i];
-        auto type = p.first;
-        const auto& args = p.second;
+using Handler = std::function<void(const std::string&, Registers&)>;
+int handle_basic_cmd(CommandType type, const std::vector<std::string>& args, Registers& registers, Handler send_handler, Handler receive_handler) {
+        int off = 0;
         switch(type) {
-            case CommandType::Sound: {
-                freq = get_val(args[0]);
+            case CommandType::Send: {
+                send_handler(args[0], registers);
             } break;
             case CommandType::Set: {
                char reg = args[0][0];
-               registers[reg] = get_val(args[1]);
+               registers[reg] = get_val(args[1], registers);
             } break;
             case CommandType::Add: {
                char reg = args[0][0];
-               registers[reg] += get_val(args[1]);
+               registers[reg] += get_val(args[1], registers);
             } break;
             case CommandType::Mul: {
                char reg = args[0][0];
-               registers[reg] *= get_val(args[1]);
+               registers[reg] *= get_val(args[1], registers);
             } break;
             case CommandType::Mod: {
                char reg = args[0][0];
-               registers[reg] %= get_val(args[1]);
+               registers[reg] %= get_val(args[1], registers);
            } break;
-            case CommandType::Recover: {
-                if(get_val(args[0]) != 0) {
-                    std::cout << freq << std::endl;
-                }
+            case CommandType::Receive: {
+                receive_handler(args[0], registers);
            } break;
            case CommandType::JumpGZ: {
-                auto arg1 = get_val(args[0]);
-                auto arg2 = get_val(args[1]);
-                if(arg1) {
-                    i += (arg2 + ((arg2 < 0) ? -1 : +1));
+                auto arg1 = get_val(args[0], registers);
+                auto arg2 = get_val(args[1], registers);
+                if(arg1 > 0) {
+                    off = arg2 - 1; 
                 }
            } break;
         }
-    }
+        return off;
 }
-struct Program {
-    void wait() { w.join(); }
-    void start() { started = true; }
 
-    Program(const Commands& commands, int i) 
-        :     other(nullptr) 
-             ,cmds(commands)
-             ,started(false)
-             ,sent(0)
-             ,rec(0)
-             ,id(i)
-             ,registers{{'p',id}}
+void part1(const Commands& cmds) {
+    Registers registers;
 
-        { 
-            w = std::thread([this](){
-                    while(!started) ;
-
-                    auto get_val = [this] (const std::string& arg) {
-                        if(std::isalpha(arg[0])) return registers[arg[0]];
-                        else return static_cast<int64_t>(std::stoll(arg));
-                    };
-
-                    for(size_t i = 0; i < cmds.size() && started; i++) {
-                        const auto& p = cmds[i];
-                        auto type = p.first;
-                        const auto& args = p.second;
-                        switch(type) {
-                        case CommandType::Sound: {
-                            other->send(get_val(args[0])); 
-                            sent++;
-                        } break;
-                        case CommandType::Set: {
-                            char reg = args[0][0];
-                            registers[reg] = get_val(args[1]);
-                        } break;
-                        case CommandType::Add: {
-                           char reg = args[0][0];
-                           registers[reg] += get_val(args[1]);
-                        } break;
-                        case CommandType::Mul: {
-                           char reg = args[0][0];
-                           registers[reg] *= get_val(args[1]);
-                        } break;
-                        case CommandType::Mod: {
-                           char reg = args[0][0];
-                           registers[reg] %= get_val(args[1]);
-                        } break;
-                        case CommandType::Recover: {
-                           auto r = recv();
-                           auto reg = args[0][0];
-                           if(r != std::numeric_limits<int64_t>::min())  {
-                               registers[reg] = r;
-                               rec++;
-                           }
-                        } break;
-                        case CommandType::JumpGZ: {
-                          auto arg1 = get_val(args[0]);
-                          auto arg2 = get_val(args[1]);
-                          if(arg1 > 0) {
-                              i += arg2;
-                              i -= 1;
-                          }
-                        } break;
-                        }
-                    }
-            });
-
+    int freq = 0;
+    bool runs = true;
+    auto snd = [&freq](const std::string& arg, Registers& regs) {
+        freq = get_val(arg, regs);
+    };
+    auto recv = [&freq, &runs](const std::string& arg, Registers& regs) {
+        if(get_val(arg, regs) != 0) {
+            std::cout << freq << std::endl;
+            runs = false;
         }
-
-    void send(int64_t val) {
-        std::lock_guard<std::mutex> guard{q_lock};
-        values.push_back(val);
-        cv.notify_one();
-    }
-
-    void print_reg() {
-        std::ostringstream ss;
-        ss << "[" << id << "]. ";
-        for(auto p : registers) {
-            ss << p.first << ": " << p.second << ", ";
+    };
+    for(size_t i = 0; i < cmds.size() && runs; i++) {
+            const auto& p = cmds[i];
+            auto type = p.first;
+            const auto& args = p.second;
+            i += handle_basic_cmd(type, args, registers, snd, recv);
         }
-        std::cout << ss.str() << std::endl;
-    }
-
-    int64_t recv() {
-        std::unique_lock<std::mutex> lk(m);
-        cv.wait_for(lk, std::chrono::milliseconds(100), [this]{ return !values.empty();});
-        if(!values.empty()) {
-            std::lock_guard<std::mutex> guard{q_lock};
-            auto b = values.front();
-            values.pop_front();
-            return b;
-        }
-        else {
-            started = false;
-            return std::numeric_limits<int64_t>::min();
-        }
-    }
-
-    std::condition_variable cv;
-    std::mutex m;
-    std::mutex q_lock;
-    std::thread w;
-    std::deque<int64_t> values;
-    Program* other;
-    Commands cmds;
-    bool started;
-    int64_t sent;
-    int64_t rec;
-    int64_t id;
-    std::map<char, int64_t> registers;
-};
+}
 
 void part2(const Commands& cmds) {
-
-    Program p1(cmds, 0);
-    Program p2(cmds, 1);
-    p1.other = &p2;
-    p2.other = &p1;
-    p1.start();
-    p2.start();
-    p1.wait();
-    p2.wait();
-    p1.print_reg();
-    p2.print_reg();
-    std::cout << "Id: " << p1.id << " s:" << p1.sent <<  " r:" << p1.rec << std::endl;
-    std::cout << "Id: " << p2.id << " s:" << p2.sent << " r:" << p2.rec << std::endl;
-}
-void part2_sync(const Commands& cmds) {
-    using Regs = std::map<char, int64_t>;
     using Vals = std::deque<int64_t>;
     size_t idx_1 = 0;
     Vals vals_1;
-    Regs registers_1{{'p', 0}};
+    Registers registers_1{{'p', 0}};
     bool waits_1 = false;
     size_t sent_1 = 0;
 
     size_t idx_2 = 0;
     Vals vals_2;
-    Regs registers_2{{'p', 1}};
+    Registers registers_2{{'p', 1}};
     bool waits_2 = false;
     size_t sent_2 = 0;
 
     bool runs = true;
-    auto step = [&runs, &cmds](size_t& i, Regs& registers, Vals& vals, bool& waits, size_t& sent, bool other_waits, Vals& other_vals) {
-        auto get_val = [&registers] (const std::string& arg) {
-            if(std::isalpha(arg[0])) return registers[arg[0]];
-            else return static_cast<int64_t>(std::stoll(arg));
-        };
+    auto step = [&runs, &cmds](size_t& i, Registers& registers, Vals& vals, bool& waits, size_t& sent, bool other_waits, Vals& other_vals) {
         const auto& p = cmds[i];
         auto type = p.first;
         const auto& args = p.second;
-        switch(type) {
-            case CommandType::Sound: {
-                other_vals.push_back(get_val(args[0]));
-                sent++;
-            } break;
-            case CommandType::Set: {
-                char reg = args[0][0];
-                registers[reg] = get_val(args[1]);
-            } break;
-            case CommandType::Add: {
-               char reg = args[0][0];
-               registers[reg] += get_val(args[1]);
-            } break;
-            case CommandType::Mul: {
-               char reg = args[0][0];
-               registers[reg] *= get_val(args[1]);
-            } break;
-            case CommandType::Mod: {
-               char reg = args[0][0];
-               registers[reg] %= get_val(args[1]);
-            } break;
-            case CommandType::Recover: {
+
+        auto snd_h = [&sent, &other_vals](const std::string& arg, Registers& registers){
+            other_vals.push_back(get_val(arg, registers));
+            sent++;
+        };
+
+        auto rcv_h = [&vals, &other_waits, &runs, &waits](const std::string& arg, Registers& registers){
                if(vals.empty()) {
                    waits = true;
                    if(other_waits)  {
@@ -266,48 +136,37 @@ void part2_sync(const Commands& cmds) {
                    }
                } else {
                    waits = false;
-                   auto reg = args[0][0];
-                   auto val = vals.front();
-                   vals.pop_front();
+                   auto reg = arg[0];
+                   auto val = vals.front(); vals.pop_front();
                    registers[reg] = val;
                }
-            } break;
-            case CommandType::JumpGZ: {
-              auto arg1 = get_val(args[0]);
-              auto arg2 = get_val(args[1]);
-              if(arg1 > 0) {
-                  i += arg2;
-                  i -= 1;
-              }
-            } break;
-        }
+        };
+
+        i += handle_basic_cmd(type, args, registers, snd_h, rcv_h);
     };
+
     for(;runs;) {
         (void) runs;
         step(idx_1, registers_1, vals_1, waits_1, sent_1, waits_2, vals_2);
-        if(!waits_1)
-            idx_1++;
+        if(!waits_1) idx_1++;
         step(idx_2, registers_2, vals_2, waits_2, sent_2, waits_1, vals_1);
-        if(!waits_2)
-            idx_2++;
+        if(!waits_2) idx_2++;
     }
     std::cout << sent_2 << std::endl;
 }
 
 int main(int, char**) {
-    bool part_1 = false;
+    bool part_1 = true;
     Commands cmds;
     for(std::string line; std::getline(std::cin, line);) {
         auto vec = split(line);
         auto cmd = string_to_cmd_type(vec[0]);
         cmds.push_back(std::make_pair(cmd, std::vector<std::string>{vec.begin() + 1, vec.end()}));
     }
+
+
     if(part_1)
-        part1(cmds);
-    else { 
-        part2(cmds);
-        part2_sync(cmds);
-    }
-    //1187
-    //5969
+        part1(cmds); //1187
+    else  
+        part2(cmds); //5969
 }
