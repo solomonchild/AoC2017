ebb18e9b45b5d5a244e015b4268c41cf2bac7030
solomonchild <solomonchild@gmail.com>
2017-12-25 21:47:02 +0200

    Finished task 24

--- cpp/src/task24.cpp
+++ cpp/src/task24.cpp
@@ -1,64 +1,106 @@
 #include <string>
 #include <iostream>
-#include <map>
+#include <set>
+#include <deque>
 #include <vector>
 #include <functional>
 #include <algorithm>
 
-struct Port {
-    int in;
-    int out;
-    Port(int in, int out)
-        : in(in), out(out) {}
-};
-using PortMap = std::map<Port, int, std::function<bool(const Port&, const Port&)>>;
-using Ports = std::vector<Port>;
+using AdjList = std::vector<unsigned>;
+using AdjLists = std::vector<AdjList>;
 
-std::ostream& operator<<(std::ostream& oss, const Port& p) {
-    oss << p.in << " " << p.out;
-    return oss;
-}
-
-PortMap read() {
-    PortMap map([](const Port& p1, const Port& p2){return p1.in < p2.in || p1.out < p2.out;});
+AdjLists read() {
+    AdjLists list;
     while(std::cin.good()) {
-        int in;
-        int out;
+        unsigned in;
+        unsigned out;
         if(!(std::cin >> in))
                 break;
         std::cin.ignore(1, '/');
         if(!(std::cin >> out))
             break;
-        Port p(in, out);
-        map[p]++;
+        if(list.size() <= in) list.resize(in+1);
+        if(list.size() <= out) list.resize(out+1);
+        list[in].push_back(out);
+        if(in != out)
+            list[out].push_back(in);
     }
-    return map;
+    return list;
 }
 
-int find_out (const Ports& ports) {
-    if(ports.size() == 1)
-        return ports[0].out;
-    auto ultimate = std::prev(ports.end());
-    auto pen_ultimate = std::prev(ports.end(), 2);
-    if(find_out(ports) == pen_ultimate->out) return pen_ultimate->in;
-    else return pen_ultimate->out;
-};
+using Path = std::deque<unsigned>;
 
-void build(Ports& ports, const Port& start, PortMap& map) {
-    auto out = find_out(ports);
-    auto it_in = std::find_if(map.begin(), map.end(), [out](const auto& p){
-            return p.second > 0 && p.first.in == start.out;
-    });
-    if(it_in != map.end())
-        std::cout << it_in->first.in;
+unsigned path_calc(const Path& p) {
+    unsigned ret = 0u;
+    for(size_t i = 0; i < p.size(); i++) {
+        ret += p[i];
+    }
+    return ret;
+}
 
+Path solve1(unsigned v, AdjLists lists) {
+    Path max;
+    for(auto u : lists[v]) {
+        auto copy = lists;
+        AdjList& vl = copy[v];
+        AdjList& ul = copy[u];
+        vl.erase(std::find(vl.begin(), vl.end(), u));
+        if(v != u)
+            ul.erase(std::find(ul.begin(), ul.end(), v));
+        auto tmp = Path{v, u};
+        auto s = solve1(u, copy);
+        tmp.insert(tmp.end(), s.begin(), s.end());
+        if(path_calc(tmp) > path_calc(max)) {
+            max = tmp;
+        }
+    }
+    return max;
+}
+
+Path solve2(unsigned v, AdjLists lists) {
+    Path max;
+    for(auto u : lists[v]) {
+        auto copy = lists;
+        AdjList& vl = copy[v];
+        AdjList& ul = copy[u];
+        vl.erase(std::find(vl.begin(), vl.end(), u));
+        if(v != u)
+            ul.erase(std::find(ul.begin(), ul.end(), v));
+        auto tmp = Path{v, u};
+        auto s = solve2(u, copy);
+        tmp.insert(tmp.end(), s.begin(), s.end());
+        if(tmp.size() > max.size()) {
+            max = tmp;
+        } else if(tmp.size() == max.size() && path_calc(tmp) > path_calc(max)) {
+            max = tmp;
+        }
+    }
+    return max;
 }
 
 int main(int, char**) {
-    auto map = read();
-    std::cout << std::endl;
-    for(auto& p : map) {
-        std::cout << p.first << std::endl;
+    auto lists = read();
+    for(size_t i = 0; i < lists.size(); i++) {
+        std::cout << i << ": ";
+        for(auto el : lists[i])
+            std::cout << el << ", ";
+        std::cout << std::endl;
+    }
+    const bool part1 = true;
+    Path max;
+    if(part1)
+        max = solve1(0, lists);
+        //1906
+    else 
+        max = solve2(0, lists);
+        //1824
+
+    std::cout << path_calc(max) << std::endl;
+    for(size_t i = 0; i < max.size(); i++) {
+        std::cout << max[i];
+        if(!(i & 0x1)) {
+            std::cout << "/";
+        } else if(i != max.size() - 1) std::cout << "--";
     }
 
 }
